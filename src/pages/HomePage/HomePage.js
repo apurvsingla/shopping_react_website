f6bc60bf5c9377b49a99700f3c867deb22174dde
Apurv <apurvsingla@gmail.com>
2020-09-06 19:20:21 +0530

    files setup and homepage

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Directory from '../../Components/Directory/Directory';
+import './HomePage.scss';
+
+export default function HomePage() {
+    return (
+        <div className="homepage">
+            <Directory />
+        </div>
+    )
+}
