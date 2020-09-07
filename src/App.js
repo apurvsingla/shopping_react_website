18156b4475bdf96aba28a0440ae4250ccfbfa4d2
Apurv <apurvsingla@gmail.com>
2020-09-07 11:17:58 +0530

    navigation added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import HomePage from './pages/HomePage/HomePage';
+import {
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return (
+  const nav = (props) => {
+    console.log(props)
+    return(
     <div>
-      <HomePage />
+      <Link to="/">Home</Link>
+      <h1>Hats</h1>
     </div>
+  )
+}
+  return (
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/hats" component={nav} />
+    </Switch>
   );
 }
 
--- src/Components/Menu_Item/Menu_item.js
+++ src/Components/Menu_Item/Menu_item.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Menu_item.scss';
 
-export default function MenuItem({title, imageUrl, size}) {
+export default function MenuItem({title, imageUrl, size, history, linkUrl, match}) {
     return (
-        <div className={`${size} menu-item`}>
+        <div className={`${size} menu-item`} 
+        onClick={() => history.push(`${match.url}${linkUrl}`)}>
             <div className="background-image" style={{
-            backgroundImage: `url(${imageUrl})`
-        }}/>
+                backgroundImage: `url(${imageUrl})`
+            }}/>
             <div className="content">
                 <h1 className="title">{title.toUpperCase()}</h1>
                 <span>Shop Now</span>
